aba5d18e9cc9308f5dc630dd5f14f42db4b70094
BackGwa <64456214+BackGwa@users.noreply.github.com>
2022-07-01 14:21:47 +0900

    개선 버전

--- 20ask.py
+++ 20ask.py
@@ -1,23 +1,20 @@
 import random
 
-def randomEvent(DefRandomValue):
+asked = 0
 
-    asked = 0
-    while(asked < 20):
-        userinput = input("수를 입력하세요. ({}/{})  >> ".format(asked + 1, 20))
-        asked += 1
-        userinput2int = int(userinput)
-        if (userinput2int == DefRandomValue):
-            print("\n입력 값과 설정 값이 일치합니다!")
-            break
-        elif (userinput2int > DefRandomValue):
-            print("\n입력 된 값은 설정된 값보다 큽니다!")
-        elif (userinput2int < DefRandomValue):
-            print("\n입력 된 값은 설정된 값보다 작습니다!")
-        else:
-            print("\n알 수 없는 오류가 발생했습니다.")
+print("\n=====[  UP & DOWN GAME  ]=====\n")
+DefRandomValue = random.randint(1, 100)
 
-    print("\n질문이 종료되었습니다!")
+while (asked < 20):
+    userinput = input("수를 입력하세요. ({}번째 시도)  >> ".format(asked + 1))
+    asked += 1
+    userinput2int = int(userinput)
+    if (userinput2int == DefRandomValue):
+        print("\n일치합니다!")
+        break
+    elif (userinput2int > DefRandomValue):
+        print("\n입력 된 값이 더 큽니다!")
+    elif (userinput2int < DefRandomValue):
+        print("\n입력 된 값이 더 적습니다!")
 
-randomValue = ("{}".format(random.randint(0, 100)))
-randomEvent(int(randomValue))
+print("질문이 종료되었습니다! | 답 : {} | 시도 횟수 : {}".format(DefRandomValue, asked + 1))
